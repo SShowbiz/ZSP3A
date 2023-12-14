8e49fb58a79d32606898ccf31bbc65bd055efa44
SShowbiz <hwkim408@snu.ac.kr>
2023-12-14 16:48:03 +0900

    feat: update project page

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,21 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
+$(document).ready(function () {
+  // Check for click events on the navbar burger icon
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
+  var options = {
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    loop: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
+  // Initialize all div with carousel class
+  var carousels = bulmaCarousel.attach('.carousel', options);
+
+  bulmaSlider.attach();
 
 })
